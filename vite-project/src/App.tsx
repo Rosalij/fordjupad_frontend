ff7aec095ae0bd60b60c1c6c77a0d69a3bc61040
Rosali <rosali.alfta@gmail.com>
2026-01-20 15:37:12 +0100

    Refactor App component to display a list of books and update BooksComponent title

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,18 +2,56 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 import BooksComponent from './components/BooksComponent.tsx'
 import HeaderComponent from './components/HeaderComponent.tsx'
+
+
 function App() {
 
+  const BookArr = [{
+    title: "Lord of the Rings",
+    author: "JR. Tolkien",
+    read: true,
+    ISBN: 1231556
+  },
+  {
+    title: "Lord of the Rings",
+    author: "JR. Tolkien",
+    read: true,
+    ISBN: 1231556
+  },
+  {
+    title: "Into The Wild",
+    author: "JR. Tolkien",
+    read: true,
+    ISBN: 1231556
+  },
+  {
+    title: "Harry Potter and the chamber of secrets",
+    author: "JK. Rowling",
+    read: true,
+    ISBN: 1231556
+  },
+  {
+    title: "The Hunger Games",
+    author: "Jennifer Lawrence",
+    read: true,
+    ISBN: 1231556
+  }
+  ]
   return (
-    <><HeaderComponent title={"My Book App"} />
- <section>
-<BooksComponent/>
- </section>
-    </>
-  )
-}
+    <> <HeaderComponent title={"My Book App"} />
+     <br />
+      {BookArr.map((book) => (
+        <BooksComponent title={book.title} author={book.author} read={book.read} ISBN={book.ISBN} />
+      ))}
 
-export default App
+     
+      <section>
+
+
+      </section>
+    </>) }
+
+
+  export default App
--- vite-project/src/components/BooksComponent.tsx
+++ vite-project/src/components/BooksComponent.tsx
@@ -8,12 +8,13 @@ import React from 'react'
 }
 
 
+
 export default function BookComponent({title, author, read, ISBN} : BookComponentProps) {
 
   return (
     <div>
   <section>
-  <h1 style={{color: "green", fontFamily:"monospace"}}>Contactcards</h1>
+  <h1 style={{color: "green", fontFamily:"monospace"}}>Book</h1>
   <p>{title} by {author}</p>
   <p>{read}</p>
   <p>{ISBN}</p>
